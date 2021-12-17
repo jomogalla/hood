55161cfa8e196afe242b057afa29312a486fc7fe
J Domo <domogalla3@gmail.com>
2021-12-17 13:26:07 -0600

    Updated to new darksky api

--- src/Services/darksky.js
+++ src/Services/darksky.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const DARKSKY_AWS_ENDPOINT = 'https://7q2ji3gg98.execute-api.us-west-2.amazonaws.com/cooler-dev/';
+const DARKSKY_AWS_ENDPOINT = 'https://gyr3n45np5.execute-api.us-west-2.amazonaws.com/Prod/forecast';
 const TIMBERLINE_LATITUDE = 45.330947;
 const TIMBERLINE_LONGITUDE = -121.709854;
 
 const DarkSky =  {
     getForecast: async function () {
-        return axios.get(`${DARKSKY_AWS_ENDPOINT}${TIMBERLINE_LATITUDE},${TIMBERLINE_LONGITUDE}`);
+        return axios.get(`${DARKSKY_AWS_ENDPOINT}/${TIMBERLINE_LATITUDE}/${TIMBERLINE_LONGITUDE}`);
     }
 }
 
