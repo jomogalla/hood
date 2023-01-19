13315c224611f39aefd27b11c0a88beb650396d0
J Domo <domogalla3@gmail.com>
2023-01-19 09:57:24 -0800

    Added more stations

--- src/stations.js
+++ src/stations.js
@@ -8,6 +8,26 @@ export const stations = [
         name: 'Mckenzie',
         triplet: '619:OR:SNTL',
         default: false,
+    },
+    {
+        name: 'Aneroid',
+        triplet: '302:OR:SNTL',
+        default: false,
+    },
+    {
+        name: 'Eilertson Meadows',
+        triplet: '464:OR:SNTL',
+        default: false,
+    },
+    {
+        name: 'Wolf Creek',
+        triplet: '873:OR:SNTL',
+        default: false,
+    },
+    {
+        name: 'Bourne',
+        triplet: '361:OR:SNTL',
+        default: false,
     }
 ]
 
